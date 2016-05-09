e254dc4a629572cec3a76dcab6864458629be137
Mark Thomson <imarkthomson@gmail.com>
2016-05-08 18:27:56 -0700

    Add docs

--- all-import/pmxi_after_xml_import.php
+++ all-import/pmxi_after_xml_import.php
@@ -2,12 +2,15 @@
 
 
 /**
- * TODO: Document and create example
+ * Called after an import is complete.
+ * 
+ * Useful for any cleanup or other task that needs to be performed after an import
  * 
  * @param $import_id
  */
 function after_xml_import($import_id) {
-
+  
+  // Perform your task
 
 }
 add_action('pmxi_after_xml_import', 'after_xml_import', 10, 1);
