56b3d76127e35a1caae2e307813c250687657c2e
Mark Thomson <imarkthomson@gmail.com>
2016-05-15 16:44:19 -0700

    Add comments

--- all-import/pmxi_after_xml_import.php
+++ all-import/pmxi_after_xml_import.php
@@ -6,6 +6,11 @@
  *
  * Useful for any cleanup or other task that needs to be performed after an import
  *
+ * For hooks to execute after a post is saved see:
+ *
+ * @see pmxi_after_post_import.php
+ * @link ./pmxi_saved_post.php
+ *
  * @param $import_id
  */
 function after_xml_import($import_id)
